b223c604cc4b48dd4adfa4fbcf91abbab79fea81
AlvinLiang <alvinwliang91@gmail.com>
2023-06-05 16:21:11 -0700

    fixed appjs

--- ghi/app/src/AutomobileForm.js
+++ ghi/app/src/AutomobileForm.js
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-function AutomobileForm(){
-}
-export default AutomobileForm
-
-
 function AutomobileForm ({ automobiles, getAutomobiles }) {
 
     return
