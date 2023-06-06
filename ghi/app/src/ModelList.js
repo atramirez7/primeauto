6af595136d7852d4f11dba608bb296aeb037e90b
AlvinLiang <alvinwliang91@gmail.com>
2023-06-05 18:37:59 -0700

    Finished model list and model form

--- ghi/app/src/ModelList.js
+++ ghi/app/src/ModelList.js
@@ -2,7 +2,30 @@
 
 function ModelList ({ models, getModels }) {
 
-    return
+    return(
+        <table className="table table-dark">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Picture Url</th>
+            <th>Manufacturer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {models.map(model => {
+            return (
+            <tr key={model.id}>
+              <td>{model.name}</td>
+              <td>{model.manufacturer.name}</td>
+              <td>{model.picture_url}</td>
+            </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+
+    )
 
 }
 
