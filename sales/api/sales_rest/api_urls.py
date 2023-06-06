1555dabb3efb8cd5720275dc705cbe6b914fd1ed
AlvinLiang <alvinwliang91@gmail.com>
2023-06-05 21:29:53 -0700

    Finished list and detail/delete for salespersons in views

--- sales/api/sales_rest/api_urls.py
+++ sales/api/sales_rest/api_urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
+from .api_views import (
+    api_list_salespeople, api_salespeople_detail
+)
 
 
 urlpatterns = [
-
+path("salespeople/", api_list_salespeople, name="api_list_salespeople"),
+path("salespeople/<int:id>/", api_salespeople_detail, name="api_salespeople_detail")
 ]
--- sales/api/sales_rest/api_views.py
+++ sales/api/sales_rest/api_views.py
@@ -6,6 +6,51 @@ import json
 from .models import Salesperson, Customer, Sale, AutomobileVO
 # Create your views here.
 
+class SalespeopleListEncoder(ModelEncoder):
+    model = Salesperson
+    properties = [
+        "first_name",
+        "last_name",
+        "employee_id",
+        "id",
+    ]
+
+
+
+
+
 @require_http_methods(["GET", "POST"])
 def api_list_salespeople(request, id=None):
-    pass
+    if request.method == "GET":
+        if id is not None:
+            salesperson = Salesperson.objects.filter(id=id)
+        else:
+            salesperson = Salesperson.objects.all()
+            return JsonResponse(
+                {"salesperson": salesperson},
+                encoder=SalespeopleListEncoder,
+            )
+    else:
+        content = json.loads(request.body)
+
+        salesperson = Salesperson.objects.create(**content)
+        return JsonResponse(
+            salesperson,
+            encoder=SalespeopleListEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["GET", "DELETE" ])
+def api_salespeople_detail(request, id):
+    if request.method == "GET":
+        salesperson = Salesperson.objects.get(id=id)
+        return JsonResponse(
+            salesperson,
+            encoder=SalespeopleListEncoder,
+            safe=False
+        )
+    else:
+        request.method == "DELETE"
+        count, _ = Salesperson.objects.filter(id=id).delete()
+        return JsonResponse({"deleted": count > 0})
