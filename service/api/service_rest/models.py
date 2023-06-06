7b81d3a399237eba1ece97720eb8895c149d2570
Dream Apitchaya <apitchayatramirez@gmail.com>
2023-06-06 12:59:46 -0400

    add initial models for service

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class AutomobileVO(models.Model):
+    vin =
+    sold = 
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ("first_name", "last_name")
