5a0d61c6d45c5c3af887fe5c317f817a2d60ea28
Dream Apitchaya <apitchayatramirez@gmail.com>
2023-06-06 13:21:19 -0400

    models service done

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 class AutomobileVO(models.Model):
-    vin =
-    sold = 
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.vin
+
+    def get_api_url(self):
+        return reverse("api_automobile", kwargs={"vin": self.vin})
+
 
 class Technician(models.Model):
     first_name = models.CharField(max_length=200)
@@ -16,3 +24,22 @@ class Technician(models.Model):
 
     class Meta:
         ordering = ("first_name", "last_name")
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=400)
+    status = models.CharField(max_length=200)
+    vin = models.CharField(max_length=17, unique=True)
+    customer = models.CharField(max_length=400)
+    vip = models.BooleanField()
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointments",
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return f"{self.date_time} / {self.customer}"
+
+    class Meta:
+        ordering = ("date_time")
