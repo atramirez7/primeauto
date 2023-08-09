1251756f556125966da8f26e7df7ded02cea9602
atramirez7 <apitchayatramirez@gmail.com>
2023-08-09 19:22:19 -0400

    add css

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,14 +1,21 @@
+import carimg from "./image/carbg.jpeg"
+import './MainPage.css';
+
 function MainPage() {
   return (
+    <>
+    <img className="carimg" src={carimg}></img>
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">CarCar</h1>
+      <h1 className="display-5 fw-bold">PrimeAuto</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
           The premiere solution for automobile dealership
           management!
         </p>
+
       </div>
     </div>
+    </>
   );
 }
 
